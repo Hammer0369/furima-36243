aac10c0e7ca84d0739091dc97e20562975bd1599
Hammer0369 <akatahama0369@gmail.com>
2021-07-31 23:41:28 +0900

    実装準備

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
+// require("turbolinks").start() // JavaScriptを正常に動作させるため画面遷移を高速化するGem"turbolinks"を無効化。
 require("@rails/activestorage").start()
 require("channels")
 

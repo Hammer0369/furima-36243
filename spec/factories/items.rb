53974422288c402a75273575bedbd743412b9714
Hammer0369 <akatahama0369@gmail.com>
2021-08-06 16:29:14 +0900

    FactoryBotとFakerの実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    name                  { Faker::Name.initials }
+    info                  { Faker::Lorem.sentence }
+    category_id           { Faker::Number.within(range: 2..11) }
+    status_id             { Faker::Number.within(range: 1..7) }
+    shipping_fee_id       { Faker::Number.within(range: 2..3) }
+    prefecture_id         { Faker::Number.within(range: 2..48) }
+    delivery_scheduled_id { Faker::Number.within(range: 2..4) }
+    price                 { Faker::Number.number(digits: 4) }
+    user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png', content_type: 'image/png')
+    end
   end
 end

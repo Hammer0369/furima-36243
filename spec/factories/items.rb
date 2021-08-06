df63c9db13dafca8bd04bc6dd9a245aa8a0bc3ec
Hammer0369 <akatahama0369@gmail.com>
2021-08-06 16:55:29 +0900

    FacotoryBot微修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     name                  { Faker::Name.initials }
     info                  { Faker::Lorem.sentence }
     category_id           { Faker::Number.within(range: 2..11) }
-    status_id             { Faker::Number.within(range: 1..7) }
+    status_id             { Faker::Number.within(range: 2..7) }
     shipping_fee_id       { Faker::Number.within(range: 2..3) }
     prefecture_id         { Faker::Number.within(range: 2..48) }
     delivery_scheduled_id { Faker::Number.within(range: 2..4) }

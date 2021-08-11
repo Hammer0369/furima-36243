4b7c583f0ca7d8dedd4bc8dbbd189d7a63b0e010
Hammer0369 <akatahama0369@gmail.com>
2021-08-11 18:36:00 +0900

    購入画面表示とフォーム情報保存の実装

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -6,10 +6,10 @@ class OrderAddress
     validates :user_id
     validates :item_id
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    varidates :city, :house_number
-    varidates :phone_number, format: {with: /^\d{10}$|^\d{11}$/, message: "number is invalid. Input only number"}
+    validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
+    validates :city, :house_number
+    validates :phone_number, format: {with: /\A\d{10,11}\z/, message: "is invalid. Input only number"}
   end
-  validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
